504fc0226ed02538e584309f5cbb8ebfc3d27166
Evgeniy Solntsev <evgsol@joom.com>
2022-04-15 00:40:07 +0700

    fix backslashes

--- server.py
+++ server.py
@@ -47,12 +47,11 @@ def map():
             if f["price"].find(" \u20ac/\u0434\u0435\u043d\u044c") > 0:
                 continue
         price = parse_price(f["price"])
-        if min_price and (int(min_price) > price):
-            continue
-        if max_price and (int(max_price) < price):
-            continue
-
         try:
+            if min_price and (int(min_price) > price):
+                continue
+            if max_price and (int(max_price) < price):
+                continue
             meters = float(f["meters"])
         except:
             continue
@@ -63,9 +62,13 @@ def map():
         if with_fireplace:
             if not ("fireplace" in f["description"] or "камин" in f["description"] or "kamīns" in f["description"]):
                 continue
+        address = escape(f["address"])
+        description = escape(f["description"])
         folium.Marker(
             location=f["coordinates"],
-            popup=folium.Popup(f"<a href=\"{BASE_URL}{f['link']}\">{f['address']}</a><br>{f['description']}", min_width=500, max_width=500),
+            popup=folium.Popup(
+                f"<a href=\"{BASE_URL}{f['link']}\">{address}</a><br>{description}",
+                min_width=500, max_width=500),
             tooltip=f"{meters} m2, {price} €/month",
         ).add_to(result_map)
 
@@ -78,3 +81,6 @@ def get_latest_date():
         dates.append(datetime.strptime(f[:-5], "%d_%m_%Y"))
     dates.sort()
     return dates[-1].strftime("%d_%m_%Y")
+
+def escape(s):
+    return "\\\\".join(s.split("\\"))
